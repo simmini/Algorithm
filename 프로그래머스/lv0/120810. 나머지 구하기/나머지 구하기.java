c2d8c1140361b09c68747018775fe42f3bbc6546
jeongmin <77092838+simmini@users.noreply.github.com>
2023-07-16 17:47:25 +0900

    [level 0] Title: 나머지 구하기, Time: 0.03 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
+++ 프로그래머스/lv0/120810. 나머지 구하기/나머지 구하기.java
@@ -1,7 +1,12 @@
 class Solution {
     public int solution(int num1, int num2) {
         int answer = -1;
-        
-        return num1%num2;
+        while(num1>=num2){
+            num1=num1-num2;
+        }
+        answer=num1;
+        //1.방법
+       //answer= num1%num2
+        return answer;
     }
 }
