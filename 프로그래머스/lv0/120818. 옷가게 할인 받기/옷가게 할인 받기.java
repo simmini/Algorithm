e648f2fd8ab6198af0d4710c3ae61d8d3162b0a1
jeongmin <77092838+simmini@users.noreply.github.com>
2023-07-16 21:00:40 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.29 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,22 @@
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+
+        if(price>=500000){
+            answer=(int)Math.floor (price*0.8);
+        }else if(price>=300000){
+        	answer=(int)Math.floor (price*0.9);
+        }else if(price>=100000){
+            answer= (int)Math.floor(price*0.95);
+        }else{
+            answer=price;
+        }
+        
+        return answer;
+    }
+}
