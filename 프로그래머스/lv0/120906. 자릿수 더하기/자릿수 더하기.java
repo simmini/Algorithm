f403fef6ced94c80f073cec107c6f15de38574e6
jeongmin <77092838+simmini@users.noreply.github.com>
2023-07-16 21:16:46 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.17 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,19 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        while(n>0) {
-        	answer+=n%10;
-        	n=n/10;
+        // while(n>0) {
+        // 	answer+=n%10;
+        // 	n=n/10;
+        // }
+        
+       //int를 String으로 변환 :Integer.toString     
+        String []s=Integer.toString(n).split("");
+        for(int i=0;i<s.length;i++) {
+        	
+            //String을 int로 변환?
+        	answer+=Integer.parseInt(s[i]);
         }
+      
         return answer;
     }
 }
