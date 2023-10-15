65045082f52a9383bd6faa8f408e68161f42557c
jeongmin <77092838+simmini@users.noreply.github.com>
2023-10-15 17:00:00 +0900

    [Bronze V] Title: A-B, Time: 212 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(a-b);
+		
+	
+	}
+
+}
