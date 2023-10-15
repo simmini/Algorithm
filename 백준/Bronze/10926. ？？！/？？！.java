1c5d15a7cb903154132014860c84c94563d800ff
jeongmin <77092838+simmini@users.noreply.github.com>
2023-10-15 17:08:59 +0900

    [Bronze V] Title: ??!, Time: 200 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,21 @@
+
+
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		String a="";
+		Scanner sc=new Scanner(System.in);
+		a=sc.next();
+		String b="";
+		b=a.concat("??!");
+		System.out.println(b);
+//		System.out.println(a+"??!");
+	
+	}
+
+}
