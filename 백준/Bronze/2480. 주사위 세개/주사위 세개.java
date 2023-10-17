ad861577f0c39479724915fbc5bf62eb550dbcfe
jeongmin <77092838+simmini@users.noreply.github.com>
2023-10-17 15:40:04 +0900

    [Bronze IV] Title: 주사위 세개, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,48 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		 int a,b,c;
+		 Scanner sc=new Scanner(System.in);
+		 
+		 a=sc.nextInt();
+		 b=sc.nextInt();
+		 c=sc.nextInt();
+		 
+		 if(a==b&&b==c) {
+			 System.out.println(10000+a*1000);
+		 }else if(a==b||b==c) {
+			 System.out.println(1000+b*100);
+		 }else if(c==a){
+			 System.out.println(1000+c*100);
+		 }else {
+			 if(a>b) {
+				if(b>c) {
+				//a>b>c
+					System.out.println(a*100);
+				}else {
+				//a>b<c
+					if(a>c) {
+						//a>c>b
+						System.out.println(a*100);
+					}else {
+						//c>a>b
+						System.out.println(c*100);
+					}
+				}
+			 }else {//b>=a
+				 if(a>c) {
+					 //b>a>c
+					 System.out.println(b*100);
+				 }else {//b>a<c
+					 if(b>c) {
+						 System.out.println(b*100);
+					 }else {
+						 System.out.println(c*100);
+					 }
+				 }
+			 }
+		 }
+	}
+}
