df279b2300e52a510553eea212a9d87dc47feae4
jeongmin <77092838+simmini@users.noreply.github.com>
2023-10-17 15:44:32 +0900

    [Bronze IV] Title: 주사위 세개, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,4 +1,5 @@
 
+
 import java.util.Scanner;
 
 public class Main {
@@ -17,31 +18,39 @@ public class Main {
 		 }else if(c==a){
 			 System.out.println(1000+c*100);
 		 }else {
-			 if(a>b) {
-				if(b>c) {
-				//a>b>c
-					System.out.println(a*100);
-				}else {
-				//a>b<c
-					if(a>c) {
-						//a>c>b
-						System.out.println(a*100);
-					}else {
-						//c>a>b
-						System.out.println(c*100);
-					}
-				}
-			 }else {//b>=a
-				 if(a>c) {
-					 //b>a>c
-					 System.out.println(b*100);
-				 }else {//b>a<c
-					 if(b>c) {
-						 System.out.println(b*100);
-					 }else {
-						 System.out.println(c*100);
-					 }
-				 }
+//			 if(a>b) {
+//				if(b>c) {
+//				//a>b>c
+//					System.out.println(a*100);
+//				}else {
+//				//a>b<c
+//					if(a>c) {
+//						//a>c>b
+//						System.out.println(a*100);
+//					}else {
+//						//c>a>b
+//						System.out.println(c*100);
+//					}
+//				}
+//			 }else {//b>=a
+//				 if(a>c) {
+//					 //b>a>c
+//					 System.out.println(b*100);
+//				 }else {//b>a<c
+//					 if(b>c) {
+//						 System.out.println(b*100);
+//					 }else {
+//						 System.out.println(c*100);
+//					 }
+//				 }
+//			 }
+			 
+			 if(a>b&&a>c) {
+				 System.out.println(a*100);
+			 }else if(b>a&&b>c){
+				 System.out.println(b*100);
+			 }else {
+				 System.out.println(c*100);
 			 }
 		 }
 	}
