31613949853de4955a1a8653e90bda99d6815c7c
jeongmin <77092838+simmini@users.noreply.github.com>
2023-10-21 19:56:48 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,57 +1,36 @@
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
+
 	public static void main(String[] args) {
-		 int a,b,c;
-		 Scanner sc=new Scanner(System.in);
-		 
-		 a=sc.nextInt();
-		 b=sc.nextInt();
-		 c=sc.nextInt();
-		 
-		 if(a==b&&b==c) {
-			 System.out.println(10000+a*1000);
-		 }else if(a==b||b==c) {
-			 System.out.println(1000+b*100);
-		 }else if(c==a){
-			 System.out.println(1000+c*100);
-		 }else {
-//			 if(a>b) {
-//				if(b>c) {
-//				//a>b>c
-//					System.out.println(a*100);
-//				}else {
-//				//a>b<c
-//					if(a>c) {
-//						//a>c>b
-//						System.out.println(a*100);
-//					}else {
-//						//c>a>b
-//						System.out.println(c*100);
-//					}
-//				}
-//			 }else {//b>=a
-//				 if(a>c) {
-//					 //b>a>c
-//					 System.out.println(b*100);
-//				 }else {//b>a<c
-//					 if(b>c) {
-//						 System.out.println(b*100);
-//					 }else {
-//						 System.out.println(c*100);
-//					 }
-//				 }
-//			 }
-			 
-			 if(a>b&&a>c) {
-				 System.out.println(a*100);
-			 }else if(b>a&&b>c){
-				 System.out.println(b*100);
-			 }else {
-				 System.out.println(c*100);
-			 }
-		 }
+
+	 int a,b,c=0;
+	// BufferedReader br=new BufferedReader(new InputStreamReader(System.in))
+	 
+	Scanner sc=new Scanner(System.in);
+	a=sc.nextInt();
+	b=sc.nextInt();
+	c=sc.nextInt();
+	int ans=0;
+	if(a==b&&b==c&&c==a) {
+		ans=10000+a*1000;
+	}else if(a==b||a==c) {
+		ans=1000+a*100;
+	}else if(b==c) {
+		ans=1000+b*100;
+	}else if(a>b&&a>c) {
+		ans=a*100;
+	}else if(b>c&&b>a) {
+		ans=b*100;
+	}else {
+		ans=c*100;
 	}
+	//ans=Math.max(Math.max(a, b), c);
+	System.out.println(ans);
+	}
+
 }
