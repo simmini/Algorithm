0b3f62b8beb7573bb1218ed9f05bd577255e5ba7
jeongmin <77092838+simmini@users.noreply.github.com>
2023-10-21 21:11:24 +0900

    [Bronze V] Title: A+B - 4, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,24 +1,27 @@
+
+
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-	public static void main(String args[]) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		String str;
- 
-		while( (str=br.readLine()) != null ){
-		    
-			st = new StringTokenizer(str," ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			sb.append(a+b).append("\n");
-		
-		}
-		System.out.print(sb);
+
+	public static void main(String[] args) throws IOException {
+
+	 int a=0;
+	 int b=0;
+	 BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	 StringBuilder sb=new StringBuilder();
+	 String str;
+	 while((str=br.readLine())!=null) {
+	 StringTokenizer st=new StringTokenizer(str," ");
+	 a=Integer.parseInt(st.nextToken());
+	 b=Integer.parseInt(st.nextToken());
+	 sb.append(a+b).append("\n");
+	 }
+	 System.out.println(sb);
+
 	}
 }
