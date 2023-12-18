6495ee30b308c402c861c03bdfbd826a314f960e
jeongmin <77092838+simmini@users.noreply.github.com>
2023-12-18 11:10:00 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 248 ms, Memory: 18544 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -19,26 +21,31 @@ public class Main {
 		for(int i=0;i<arr.length;i++) {
 			arr[i]=i+1;
 		}
-		
+	
 		
 		for(int i=0;i<m;i++) {
 			int j=sc.nextInt();//1
 			int k=sc.nextInt();//4
+			int first=j-1;
 			int last=k-1;//3
 		
- 			for(int t=j-1;t<=k-1;t++) {//2~3
- 				if(last<=t) {//3
-					break;
-				}
-   				int tmp=arr[t];
-				arr[t]=arr[last];
- 				arr[last]=tmp;
-				last--;//2
-				
+// 			for(int t=j-1;t<=k-1;t++) {//2~3
+// 				if(last<=t) {//3
+//					break;
+//				}
+			while(first<last) {
+   				int tmp=arr[first];
+				arr[first++]=arr[last];
+ 				arr[last--]=tmp;
+			
 			}
-		}
+			}
+		
 		
 		for(int i=0;i<arr.length;i++) {
 			System.out.print(arr[i]+" ");
 		}
-    }}
+
+	}
+		
+} 
