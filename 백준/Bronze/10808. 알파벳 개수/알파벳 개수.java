02fe6677efed46ece890c7ea4b918d84fda8771a
jeongmin <77092838+simmini@users.noreply.github.com>
2024-12-06 10:54:41 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 164 ms, Memory: 17600 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -4,32 +4,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        String a=sc.next();
-        int alp[]=new int[26];
-        char[] ch =a.toCharArray();
-        //abcd
-        // efgh
-        // ijkl
-        // mnop
-        // qrst
-        // uvwx
-        // yz
-        int answer[]=new int[26];
-        for(int j=0;j<ch.length;j++){
-            for(int i=0;i<26;i++){
-                if(ch[j]==97+i){
-                    answer[i]++;
-                    break;
-                }
-            }
-        }
-        for(int i=0;i<26;i++){
-            System.out.print(answer[i]+" ");
-        }
+       Scanner sc=new Scanner(System.in);
+       String str=sc.next();
+       int answer[]=new int[26];
+       char a[]=str.toCharArray();
+       for(int i=0;i<a.length;i++){
+            answer[a[i]-'a']++;
+       }
 
-        //System.out.println(Arrays.toString(answer));
-
+       for (int i=0;i<26;i++){
+           System.out.println(answer[i]);
+       }
 
     }
 }
