11929b6e14b51726fb33cbe377f4e31201a44208
jeongmin <77092838+simmini@users.noreply.github.com>
2025-05-30 16:49:06 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,45 +1,49 @@
-import java.io.*;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class Main {
-    static boolean[][]graph;
-    static boolean[]visited;
     static int n,m;
+    static boolean graph[][];
+    static boolean visited[];
     static int answer;
 
     public static void dfs(int index){
         visited[index] = true;
-        IntStream.range(0,n).forEach(i->{
+        for (int i=0;i<n;i++){
             if (!visited[i]&&graph[index][i]){
                 dfs(i);
             }
-        });
+        }
         answer++;
     }
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
         n=Integer.parseInt(br.readLine());
         m=Integer.parseInt(br.readLine());
+
 
         graph=new boolean[n][n];
         visited=new boolean[n];
         int x,y;
         for(int i=0;i<m;i++){
             StringTokenizer st=new StringTokenizer(br.readLine());
-            x=Integer.parseInt(st.nextToken())-1;
-            y=Integer.parseInt(st.nextToken())-1;
+             x=Integer.parseInt(st.nextToken())-1;
+             y=Integer.parseInt(st.nextToken())-1;
+
             graph[x][y]=true;
             graph[y][x]=true;
 
         }
+
         dfs(0);
         System.out.println(answer-1);
         br.close();
 
 
-
     }
+
 }
